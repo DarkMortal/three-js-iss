bd5dac91ad25ac85ef1722eb6b0da7ca21c38295
Dark Mortal <chodab74@gmail.com>
2022-10-01 20:44:44 +0530

    Added ISS Model

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import "./style.css";
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
+//import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
 
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
 import * as THREE from "three";
@@ -22,17 +23,8 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 // document.body.appendChild(renderer.domElement);
 camera.position.setZ(25); //! set it to 25 to see the entire earth
 camera.position.setX(0); //! set it to 0 to see the entire earth (puts it in the center of screen)
-// camera.position.setY(5)
-// camera.zoom(0.5);
-// !torus shape
-// const geometry = new THREE.TorusGeometry(10, 3, 16, 80);
-// const material = new THREE.MeshStandardMaterial({ color: 0xff6347 });
-// const cube = new THREE.Mesh(geometry, material);
-// cube.position.setX(-20);
-// cube.position.setZ(10);
-// scene.add(cube);
+camera.position.setY(5)
 
-// camera.position.z = 5;
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
 scene.add(ambientLight);
 // !point light // like a bulb or some powerful lightsourse
@@ -45,23 +37,6 @@ const directionalLight = new THREE.DirectionalLight(0xffffff, 2);
 directionalLight.position.set(50,20,4)
 directionalLight.target.position.set(0,0,0)
 scene.add(directionalLight);
-// const helper = new THREE.PointLightHelper(pointLight)
-// scene.add(helper)
-// !cloudtexture
-const cloudtexture = new THREE.TextureLoader().load(
-  "./ear0xuu2.jpg/storm_clouds.jpg"
-);
-// const cloud = new THREE.Mesh(
-//   new THREE.SphereGeometry(10.1, 32, 32),
-//   new THREE.MeshPhongMaterial({
-//     map: cloudtexture,
-//     alphaTest: 0.2,
-//     opacity: 0.4,
-//     transparent: true,
-//   })
-// );
-// cloud.position.setZ(6);
-// scene.add(cloud);
 
 // //! earth texture
 const normalTexture = new THREE.TextureLoader().load(
@@ -75,8 +50,7 @@ const earth = new THREE.Mesh(
   new THREE.MeshPhongMaterial({
     map: earthtextture,
     normalMap: normalTexture,
-    // bumpMap: cloudtexture,
-    // bumpScale: 0.005,
+    //bumpScale: 12,
   })
 );
 earth.position.setZ(0);
@@ -88,33 +62,33 @@ earth.rotateY(-4.7)
 const space = new THREE.TextureLoader().load("./tycho8.jpg/tycho8.jpg");
 scene.background = space;
 
-// const iss = new THREE.ImageLoader().load("./ear0xuu2.jpg/ISS1.jpg", (image)=> {
-//   const canvas = document.createElement("bg");
-//   const context = canvas.getContext("2d");
-//   context.drawImage(image, 100, 100);
-// });
-
-const box = new THREE.BoxGeometry(2, 2 , 1);
-// const boxtexture = new THREE.TextureLoader()
-const boxtexture = new THREE.MeshStandardMaterial({ color: "0xff6347" });
-const boxMesh = new THREE.Mesh(box,boxtexture)
-// boxMesh.position.set(-15, 3, 15)
-
-
-
 const controls = new OrbitControls(camera, renderer.domElement);
+const iss = new THREE.Group();
 
+//Draco Loader
+const dracoLoader = new DRACOLoader();
+dracoLoader.setDecoderPath('./draco/');
+
+//GLTF Loader
+const gltfLoader = new GLTFLoader();
+gltfLoader.setDRACOLoader(dracoLoader);
+
+//ISS Model
+gltfLoader.load(
+  './iss/issDraco.gltf',
+  (gltf) => {
+    gltf.scene.scale.set(0.3, 0.3, 0.3);
+    iss.add(gltf.scene);
+    scene.add(iss);
+  },
+);
 
 //!animate fucntion to rortate the earth and clouds
 function animate() {
   requestAnimationFrame(animate);
-  // cube.rotation.x += 0.02;
-  // earth.rotation.y += 0.01;
-  // cloud.rotateY(0.01);
   renderer.render(scene, camera); //! 4. Renderer function that takes scene and camera as the argument
   controls.update();
 }
-
 
 //* Loading the 3d model 
 
@@ -159,13 +133,9 @@ const getSateliteData = async () => {
     lon:resp.iss_position.longitude,
     radius:12,
   });
-boxMesh.position.set(issPos.x, issPos.y, issPos.z)
-// earth.position.set(issPos.x, issPos.y, issPos.z);
-// camera.position.set(issPos.x, issPos.y, issPos.z);
-scene.add(boxMesh);
-
+iss.position.set(issPos.x, issPos.y, issPos.z);
+scene.add(iss);
 };
-
 
 setInterval(() => {
 getSateliteData();
--- utils/calcPosFromLatLong.js
+++ utils/calcPosFromLatLong.js
@@ -8,14 +8,5 @@ export default function calcPosFromLatLonRad({ lat, lon, radius }) {
   const z = (radius * Math.sin(phi) * Math.sin(theta));
   const y = (radius * Math.cos(phi));
 
-  // let latitude_rad = (lat * Math.PI) / 180;
-  // let longitude_rad = (lon * Math.PI) / 180;
-
-  // let xPos = radius * Math.cos(latitude_rad) * Math.cos(longitude_rad);
-  // let zPos = radius * Math.cos(latitude_rad) * Math.sin(longitude_rad);
-  // let yPos = radius * Math.sin(latitude_rad);
-
-  // return { x: xPos, y: yPos, z: zPos };
-
   return { x, y, z };
 }
