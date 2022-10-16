c59add84822b65ac711488c95dcddfc7b190f94d
DarkMortal <chodab74@gmail.com>
2022-10-16 20:24:20 +0530

    Minor bug fixes

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles.css';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
